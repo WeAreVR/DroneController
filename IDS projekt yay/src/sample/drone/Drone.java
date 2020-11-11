52fe4993e67718b6a5b6acec5f87750c9c62b0c4
Tobias Dolma Eichler <47886207+Braeze@users.noreply.github.com>
2020-11-11 17:35:07 +0100

    Revert "Revert "Add files via upload""
    This reverts commit 9848e935e2c5a8c06e72c67c3d150bb390e03c31.

---
+++ IDS projekt yay/src/sample/drone/Drone.java
@@ -0,0 +1,15 @@
+package sample.drone;
+import javafx.scene.canvas.GraphicsContext;
+import java.awt.*;
+
+public class Drone {
+
+    public void draw(GraphicsContext graphicsContext){
+        //graphicsContext.strokeRect();
+    }
+
+    public Drone getCopy(){
+        return new Drone();
+    }
+
+}
